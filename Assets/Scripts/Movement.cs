caf2b497119d3a22178dead2486e6ca707bebb84
GameJarne <katpoes373.devolder@gmail.com>
2022-12-04 17:18:53 +0100

    Initial Commit 🚀

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 12f;
+    float horizontalMovement;
+    Rigidbody2D rb;
+
+    [Header("Jumping")]
+    [SerializeField] float jumpForce = 20f;
+    bool jumped = false;
+
+    [Header("Ground Check")]
+    [SerializeField] LayerMask groundMask;
+    BoxCollider2D playerCollider;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        if (!jumped && Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            jumped = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+
+        if (jumped)
+        {
+            jumped = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, Vector2.down, .1f, groundMask);
+    }
+}
